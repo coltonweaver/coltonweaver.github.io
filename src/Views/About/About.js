e3efce97ce522b8630a6ac5d46f400c8577934c2
Colton Weaver <coltonbweaver@gmail.com>
2018-08-29 12:19:39 -0500

    Added verification for google, changed out style of pages.

--- src/Views/About/About.js
+++ src/Views/About/About.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Header from '../../General/Header/Header';
+import ColtonPic from '../../Images/colton.jpg';
 import './About.css';
 
 class About extends Component {
     render() {
         return(
             <div>
-                <Header image="" />
+                <Header image={ColtonPic} />
                 <div>TODO</div>
             </div>
         );
--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
-import Header from '../../General/Header/Header';
-import HomeBanner from '../../Images/HomeBanner.png';
 import './Home.css';
 
 class Home extends Component {
     render() {
         return(
             <div className="Home">
-                <Header image={HomeBanner} />
-                <h1 className="HomeTitle">Welcome to my Website</h1>
+                <h1 className="HomeTitle"></h1>
                 <h2 className="HomeContent">
-                    Todo: Implement View
                 </h2>
             </div>
         );
