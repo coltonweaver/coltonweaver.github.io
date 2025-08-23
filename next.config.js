12e550681362d937cd340a07897bf9710f847a61
Colton Weaver <me@coltonweaver.com>
2025-08-22 21:30:57 -0400

    Fixed relative path

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/coltonweaver.github.io',
+  assetPrefix: '/coltonweaver.github.io',
   images: {
     unoptimized: true
   }
